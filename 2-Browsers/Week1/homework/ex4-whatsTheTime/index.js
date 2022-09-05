96711b3fb5d910d343f6e538edc87b78fca619e8
Vladimir Seredinin <vosem.nl@gmail.com>
2022-09-05 18:13:58 +0200

    I fixed up /for ... of/

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -7,20 +7,17 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
   second). Use `setInterval()` to make sure the time stays current.
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
-function addCurrentTime() {
-  const currentTime = document.getElementById('currentTime')
-  if (currentTime) {
-    currentTime.textContent = new Date().toLocaleTimeString(); 
-   } else {
-    const noTime = document.createElement('h1');
-    noTime.id = 'currentTime';
-    noTime.style.textAlign = 'center';
-    document.body.appendChild(noTime);
-    addCurrentTime();
-    setInterval(addCurrentTime, 1000);
-  }    
+function addCurrentTime(timeElement) {
+  timeElement.textContent = new Date().toLocaleTimeString();
 }
-  
 
+function main() {
+  const currentTime = document.createElement('h1');
+  currentTime.id = 'currentTime';
+  currentTime.style.textAlign = 'center';
+  document.body.appendChild(currentTime);
+  addCurrentTime(currentTime);
+  setInterval(() => addCurrentTime(currentTime), 1000);
+}
 
-window.addEventListener('load', addCurrentTime);
+window.addEventListener('load', main);
